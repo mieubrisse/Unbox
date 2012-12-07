fb9b1ba95eb4b93da9488cb7b1fe646e0288e267
Kevin Today <thetallmonkey@gmail.com>
2012-12-07 05:51:51 -0600

    Deleted old Bash script files

--- dropconfig_vars.sh
+++
@@ -1,9 +0,0 @@
-CONFIG_FILE="$HOME/.dropconfig"
-CONFIG_DIR="$HOME/Dropbox/DropConfig/"
-
-# Color Information
-SUCCESS_TEXT_COLOR="\e[0;32m"
-INFO_TEXT_COLOR="\e[0;33m"
-ERROR_TEXT_COLOR="\e[0;31m"
-PATH_TEXT_COLOR="\e[0;36m"
-DEFAULT_TEXT_COLOR="\e[00m"
